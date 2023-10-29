3c935e5263a81111831fda219c2b6d5169e64d65
Owen Ye <owen2608@berkeley.edu>
2023-10-28 19:21:10 -0700

    this looks terrible

--- my-react-app/src/pages/availability.jsx
+++ my-react-app/src/pages/availability.jsx
@@ -7,8 +7,19 @@ function Availability() {
   const { data, updateData } = useContext(AppContext);
   const [inputText, setInputText] = useState('');
 
-  const rows = 14;
+  const rows = 24;
   const columns = 7;
+  const times = Array.from({ length: rows }, (_, index) => {
+    let hour = Math.floor(index / 2); // starts at 0:00
+    const minute = index % 2 === 0 ? '00' : '30';
+    const ampm = hour >= 12 ? 'PM' : 'AM';
+    
+    hour = hour % 12;
+    hour = hour ? hour : 12; // the hour '0' should be '12'
+  
+    return `${hour}:${minute} ${ampm}`;
+  });
+
   const [selectedBoxes, setSelectedBoxes] = useState(
     Array.from({ length: rows }, () => Array(columns).fill(false))
   );
@@ -39,25 +50,48 @@ function Availability() {
   return (
     <>
       <div>
+      <p style={{ marginTop: '20px' }}></p>
+
         <p>Availability</p>
-        <div
-          style={{
-            display: 'grid',
-            gridTemplateColumns: `repeat(${columns}, 1fr)`,
-          }}
-        >
-          {selectedBoxes.map((row, rowIndex) => {
-            return row.map((selected, colIndex) => {
-              return (
-                <SelectableBox
-                  key={`${rowIndex}-${colIndex}`}
-                  selected={selected}
-                  onClick={() => handleBoxClick(rowIndex, colIndex)}
-                />
-              );
-            });
-          })}
+        <div style={{ display: 'flex' }}>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-end',
+              marginRight: '10px',
+            }}
+          >
+            {times.map((time) => (
+              <div
+                key={time}
+                style={{ height: '30px', display: 'flex', alignItems: 'center' }}
+              >
+                {time}
+              </div>
+            ))}
+          </div>
+          <div
+            style={{
+              display: 'grid',
+              gridTemplateColumns: `repeat(${columns}, 1fr)`,
+              gridTemplateRows: `repeat(${rows}, 1fr)`,
+            }}
+          >
+            {selectedBoxes.map((row, rowIndex) => {
+              return row.map((selected, colIndex) => {
+                return (
+                  <SelectableBox
+                    key={`${rowIndex}-${colIndex}`}
+                    selected={selected}
+                    onClick={() => handleBoxClick(rowIndex, colIndex)}
+                  />
+                );
+              });
+            })}
+          </div>
         </div>
+        
         <input
           type="text"
           name="availability"
@@ -66,10 +100,10 @@ function Availability() {
         />
         <br />
         <button>
-          <Link to='/interests'>Previous</Link>
+          <Link to="/interests">Previous</Link>
         </button>
         <button onClick={handleButtonClick}>
-          <Link to='/coursework'>Next</Link>
+          <Link to="/coursework">Next</Link>
         </button>
       </div>
     </>
